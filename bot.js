d0d0909c950b99ca62a29d75ce12bb30e000b46b
arslancovington <arslancovington@gmail.com>
2026-08-15 12:59:03 +0500

    Update static file serving and add root route

--- bot.js
+++ bot.js
@@ -9,7 +9,11 @@ const bot = new TelegramBot(TOKEN, { polling: true });
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 let users = {};
 let marketItems = [];
